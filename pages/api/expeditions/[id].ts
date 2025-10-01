5f33e7f371d5ffd2b798c5ea47f2ceb713776c5c
NicolasCavalcanti <86693553+NicolasCavalcanti@users.noreply.github.com>
2025-10-01 13:35:35 -0400

    Improve API connectivity resilience

--- pages/api/expeditions/[id].ts
+++ pages/api/expeditions/[id].ts
@@ -18,8 +18,8 @@ function toNumber(value: string | string[] | undefined): number | null {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept')
 
   if (req.method === 'OPTIONS') {
     res.status(204).end()
